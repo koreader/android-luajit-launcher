3bdf92c1272d969af5f419492edf760aa2947cf3
Frans de Jonge <fransdejonge@gmail.com>
2017-04-02 22:40:34 +0200

    ShellCheck issues on mk-luajit.sh

--- mk-luajit.sh
+++ mk-luajit.sh
@@ -12,11 +12,11 @@ fi
 # NDKABI=19  # Android 4.4+
 NDKABI=9  # Android 4.0+
 BUILD_ARCH=linux-$(uname -m)
-DEST=$(cd $(dirname $0) && pwd)/jni/luajit-build/$1
+DEST=$(cd "$(dirname "$0")" && pwd)/jni/luajit-build/$1
 
 echo "Using NDKABI ${NDKABI}."
 
-NDKVER=`grep -oP 'r\K([0-9]+)(?=[a-z])' ${NDK}/CHANGELOG.md`
+NDKVER=$(grep -oP 'r\K([0-9]+)(?=[a-z])' ${NDK}/CHANGELOG.md)
 echo "Detected NDK version ${NDKVER}..."
 if [ "$NDKVER" -lt 11 ]; then
     echo 'NDK not of the right version, please update to NDK version 11 or higher.'
@@ -29,33 +29,33 @@ clean)
 	;;
 armeabi)
 	# Android/ARM, armeabi (ARMv5TE soft-float)
-	TCVER=$NDK/toolchains/arm-linux-androideabi-4.*
-	NDKP=$TCVER/prebuilt/$BUILD_ARCH/bin/arm-linux-androideabi-
+	TCVER=($NDK/toolchains/arm-linux-androideabi-4.*)
+	NDKP=${TCVER[0]}/prebuilt/$BUILD_ARCH/bin/arm-linux-androideabi-
 	NDKF="--sysroot $NDK/platforms/android-$NDKABI/arch-arm"
 	rm -rf "$DEST"
-	make -C luajit-2.0 install HOST_CC="gcc -m32" CROSS=$NDKP TARGET_FLAGS="$NDKF" DESTDIR="$DEST" PREFIX=
+	make -C luajit-2.0 install HOST_CC="gcc -m32" CROSS="$NDKP" TARGET_FLAGS="$NDKF" DESTDIR="$DEST" PREFIX=
 	;;
 armeabi-v7a)
 	# Android/ARM, armeabi-v7a (ARMv7 VFP)
-	TCVER=$NDK/toolchains/arm-linux-androideabi-4.*
-	NDKP=$TCVER/prebuilt/$BUILD_ARCH/bin/arm-linux-androideabi-
+	TCVER=($NDK/toolchains/arm-linux-androideabi-4.*)
+	NDKP=${TCVER[0]}/prebuilt/$BUILD_ARCH/bin/arm-linux-androideabi-
 	NDKF="--sysroot $NDK/platforms/android-$NDKABI/arch-arm"
 	NDKARCH="-march=armv7-a -mfloat-abi=softfp -Wl,--fix-cortex-a8"
-	make -C luajit-2.0 install HOST_CC="gcc -m32" CROSS=$NDKP TARGET_FLAGS="$NDKF $NDKARCH" DESTDIR="$DEST" PREFIX=
+	make -C luajit-2.0 install HOST_CC="gcc -m32" CROSS="$NDKP" TARGET_FLAGS="$NDKF $NDKARCH" DESTDIR="$DEST" PREFIX=
 	;;
 mips)
 	# Android/MIPS, mips (MIPS32R1 hard-float)
-	TCVER=$NDK/toolchains/mipsel-linux-android-4.*
-	NDKP=$TCVER/prebuilt/$BUILD_ARCH/bin/mipsel-linux-android-
+	TCVER=($NDK/toolchains/mipsel-linux-android-4.*)
+	NDKP=${TCVER[0]}/prebuilt/$BUILD_ARCH/bin/mipsel-linux-android-
 	NDKF="--sysroot $NDK/platforms/android-$NDKABI/arch-mips"
-	make -C luajit-2.0 install HOST_CC="gcc -m32" CROSS=$NDKP TARGET_FLAGS="$NDKF" DESTDIR="$DEST" PREFIX=
+	make -C luajit-2.0 install HOST_CC="gcc -m32" CROSS="$NDKP" TARGET_FLAGS="$NDKF" DESTDIR="$DEST" PREFIX=
 	;;
 x86)
 	# Android/x86, x86 (i686 SSE3)
-	TCVER=$NDK/toolchains/x86-4.*
-	NDKP=$TCVER/prebuilt/$BUILD_ARCH/bin/i686-linux-android-
+	TCVER=($NDK/toolchains/x86-4.*)
+	NDKP=${TCVER[0]}/prebuilt/$BUILD_ARCH/bin/i686-linux-android-
 	NDKF="--sysroot $NDK/platforms/android-$NDKABI/arch-x86"
-	make -C luajit-2.0 install HOST_CC="gcc -m32" CROSS=$NDKP TARGET_FLAGS="$NDKF" DESTDIR="$DEST" PREFIX=
+	make -C luajit-2.0 install HOST_CC="gcc -m32" CROSS="$NDKP" TARGET_FLAGS="$NDKF" DESTDIR="$DEST" PREFIX=
 	;;
 *)
 	echo 'specify one of "armeabi", "armeabi-v7a", "mips", "x86" or "clean" as first argument'
