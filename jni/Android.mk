3b516a0fbf802d730195078ff5af2a9766f93922
Frans de Jonge <fransdejonge@gmail.com>
2017-10-20 19:43:09 +0000

    Switch from 6-year-old compress-deflate7 to upstream 7z mirror (#71)
    This should fix the x86 Android build on Gitlab.

--- jni/Android.mk
+++ jni/Android.mk
@@ -25,17 +25,19 @@ include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 
-DEFLATE7_DIR := $(LOCAL_PATH)/compress-deflate7
+DEFLATE7_DIR := $(LOCAL_PATH)/7z
 PATCH_OUTPUT := $(shell cd $(DEFLATE7_DIR) && patch -N -p1 < ../7zMain.patch)
 lzma_SOURCES := \
         7zStream.c 7zFile.c Ppmd7Dec.c Ppmd7.c Bcj2.c \
-        Bra86.c Bra.c CpuArch.c Lzma2Dec.c LzmaDec.c 7zIn.c 7zDec.c \
+        Bra86.c BraIA64.c Bra.c CpuArch.c Lzma2Dec.c LzmaDec.c 7zDec.c \
         7zCrcOpt.c 7zCrc.c 7zBuf2.c 7zBuf.c 7zAlloc.c \
+        7zArcIn.c \
+        Delta.c \
         Util/7z/7zMain.c
 
 LOCAL_MODULE := lzma
-LOCAL_SRC_FILES := $(addprefix compress-deflate7/7zip/C/, $(lzma_SOURCES))
-LOCAL_CFLAGS := -O2 -I$(DEFLATE7_DIR)/7zip/C -D_7ZIP_PPMD_SUPPPORT
+LOCAL_SRC_FILES := $(addprefix 7z/C/, $(lzma_SOURCES))
+LOCAL_CFLAGS := -O2 -I$(DEFLATE7_DIR)/C -D_7ZIP_PPMD_SUPPPORT
 
 include $(BUILD_SHARED_LIBRARY)
 
