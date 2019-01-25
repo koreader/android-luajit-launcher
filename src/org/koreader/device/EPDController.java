6b37ec419b825c67c07ef5348810273986bc1942
Martín Fernández <paziusss@gmail.com>
2019-01-25 03:20:21 +0100

    import epd device abstraction from https://github.com/unwmun/refreshU
    And do not reinvent the wheel. Thanks to the author.

---
+++ src/org/koreader/device/EPDController.java
@@ -0,0 +1,12 @@
+package com.unw.device.epdcontrol;
+
+import android.view.View;
+
+/**
+ * Created by unw on 15. 3. 31..
+ */
+public interface EPDController {
+
+    void setEpdMode(View targetView, String epdMode);
+
+}
