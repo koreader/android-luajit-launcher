523e8fbb4050512f16b7a68fae8867ce13fb1caa
Martín Fernández <paziusss@gmail.com>
2019-02-03 20:43:32 +0100

    exit after crashing, to avoid unknown states

--- jni/android-main.c
+++ jni/android-main.c
@@ -117,5 +117,7 @@ void android_main(struct android_app* state) {
     lua_close(L);
 
 quit:
+    LOGE("Destroying Native Activity due to previous errors");
     ANativeActivity_finish(state->activity);
+    exit(1);
 }
