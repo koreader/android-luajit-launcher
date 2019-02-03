cd6cd6b2bd5824e46e9d4b2113cd8dae5dc818df
Frans de Jonge <fransdejonge@gmail.com>
2019-02-03 21:09:52 +0100

    Update jni/android-main.c
    Co-Authored-By: pazos <paziusss@gmail.com>

--- jni/android-main.c
+++ jni/android-main.c
@@ -117,7 +117,7 @@ void android_main(struct android_app* state) {
     lua_close(L);
 
 quit:
-    LOGE("Destroying Native Activity due to previous errors");
+    LOGE("Destroying NativeActivity due to previous errors");
     ANativeActivity_finish(state->activity);
     exit(1);
 }
