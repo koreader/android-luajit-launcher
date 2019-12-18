44d97bd497af92809103b9b57e49b93a37c183a6
hcors <51340662+hcors@users.noreply.github.com>
2019-12-18 11:40:39 -0500

    Update DeviceInfo.kt  (#204)
    * add support for "bnrv510"

--- app/src/org/koreader/launcher/DeviceInfo.kt
+++ app/src/org/koreader/launcher/DeviceInfo.kt
@@ -139,7 +139,7 @@ internal object DeviceInfo {
 
         // Nook Glowlight 3 et al.
         NOOK_V520 = (MANUFACTURER.contentEquals("barnesandnoble") || MANUFACTURER.contentEquals("freescale"))
-                && (MODEL.contentEquals("bnrv520") || MODEL.contentEquals("bnrv700") || MODEL.startsWith("ereader"))
+                && (MODEL.contentEquals("bnrv510") || MODEL.contentEquals("bnrv520") || MODEL.contentEquals("bnrv700") || MODEL.startsWith("ereader"))
         deviceMap[EinkDevice.NOOK_V520] = NOOK_V520
 
         // Sony DPT-RP1
