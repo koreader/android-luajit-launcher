f7699b3bda8ed1222c36d11956371a6f73c36081
Martín Fernández <paziusss@gmail.com>
2020-01-06 17:18:33 +0100

    disable large heap, log available managed memory for the VM

--- app/src/org/koreader/launcher/MainApp.kt
+++ app/src/org/koreader/launcher/MainApp.kt
@@ -2,6 +2,8 @@ package org.koreader.launcher
 
 import java.io.File
 
+import android.app.ActivityManager
+import android.content.Context
 import android.content.pm.ApplicationInfo
 import android.os.Environment
 import android.util.Log
@@ -22,6 +24,8 @@ class MainApp : android.app.Application() {
         private lateinit var library_path: String
         private var debuggable: Boolean = false
         private var is_system_app: Boolean = false
+        private var large_heap: Boolean = false
+        private var managed_heap: Int = 0
     }
 
     override fun onCreate() {
@@ -33,15 +37,20 @@ class MainApp : android.app.Application() {
     /* app info into a String */
     private fun formatAppInfo(): String {
         val sb = StringBuilder(400)
-        sb.append("Application info {\n  Flags: ")
+        sb.append("Application info {")
+        sb.append("\n  VM heap: ")
+            .append(managed_heap)
+            .append("MB ")
+            .append(if (large_heap) "(large)" else "(normal)")
+        sb.append("\n  Flags: ")
         sb.append(if (is_system_app) "system" else "user")
         sb.append(if (LEGACY_STORAGE) ", legacy" else ", scoped").append(" storage")
         sb.append(if (debuggable) ", debuggable" else ".")
         sb.append("\n  Paths {")
-                .append("\n    Assets: ").append(assets_path)
-                .append("\n    Library: ").append(library_path)
-                .append("\n    Storage: ").append(storage_path)
-                .append("\n  }\n}")
+            .append("\n    Assets: ").append(assets_path)
+            .append("\n    Library: ").append(library_path)
+            .append("\n    Storage: ").append(storage_path)
+            .append("\n  }\n}")
         return sb.toString()
     }
 
@@ -49,6 +58,7 @@ class MainApp : android.app.Application() {
     private fun getAppInfo() {
         try {
             val pm = packageManager
+            val am = this.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
             val pi = pm.getPackageInfo(packageName, 0)
             val ai = pm.getApplicationInfo(packageName, 0)
 
@@ -67,6 +77,10 @@ class MainApp : android.app.Application() {
 
             if (ai.flags and ApplicationInfo.FLAG_DEBUGGABLE != 0) debuggable = true
             if (ai.flags and ApplicationInfo.FLAG_SYSTEM == 1) is_system_app = true
+            if (ai.flags and ApplicationInfo.FLAG_LARGE_HEAP != 0) large_heap = true
+
+            managed_heap = getManagedMemory(am)
+
         } catch (e: Exception) {
             /* early exception, never reached.
                Use "unknown" to let the user face the crash via logcat */
@@ -74,6 +88,15 @@ class MainApp : android.app.Application() {
             assets_path = UNKNOWN_STRING
             library_path = UNKNOWN_STRING
             storage_path = UNKNOWN_STRING
+        }
+    }
+
+    /* get managed heap size */
+    private fun getManagedMemory(am: ActivityManager): Int {
+        return if (large_heap) {
+            am.largeMemoryClass
+        } else {
+            am.memoryClass
         }
     }
 }
