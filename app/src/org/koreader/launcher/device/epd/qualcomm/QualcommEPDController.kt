048ecf274347ea8abca2b4dead8dd3f84fd0817c
Galunid <karolek1231456@gmail.com>
2020-10-20 00:00:24 +0200

    Add comments

--- app/src/org/koreader/launcher/device/epd/qualcomm/QualcommEPDController.kt
+++ app/src/org/koreader/launcher/device/epd/qualcomm/QualcommEPDController.kt
@@ -2,13 +2,16 @@ package org.koreader.launcher.device.epd.qualcomm
 
 import java.lang.Thread
 
-import android.util.Log
 import android.view.View
 
 import org.koreader.launcher.interfaces.EPDInterface
 
+// More information including epd mode values
+// https://github.com/koreader/android-luajit-launcher/pull/250#issuecomment-711443457
 class QualcommEPDController : EPDInterface {
     fun preventSystemRefresh(){
+        // Sets UpdateMode and UpdateScheme to None
+        // this function is called EpdController.setSystemUpdateModeAndScheme in onyxsdk
         Class.forName("android.view.View").getMethod("setWaveformAndScheme",
             Integer.TYPE,
             Integer.TYPE,
@@ -19,11 +22,11 @@ class QualcommEPDController : EPDInterface {
                             mode: Int, delay: Long,
                             x: Int, y: Int, width: Int, height: Int, epdMode: String?)
     {
-
         preventSystemRefresh()
         object: Thread(){
             override fun run(){
                 Thread.sleep(delay)
+                // EpdController.refreshScreenRegion in onyxsdk
                 Class.forName("android.view.View").getMethod("refreshScreen",
                     Integer.TYPE,
                     Integer.TYPE,
