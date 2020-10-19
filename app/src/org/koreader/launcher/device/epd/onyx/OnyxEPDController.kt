ba84ed4da050e09382c2e0b777d65647d5add540
Galunid <karolek1231456@gmail.com>
2020-10-19 17:55:39 +0200

    Remove bloat

--- app/src/org/koreader/launcher/device/epd/onyx/OnyxEPDController.kt
+++ app/src/org/koreader/launcher/device/epd/onyx/OnyxEPDController.kt
@@ -1,31 +1,3 @@
-/* generic EPD Controller for Android,
- *
- * interface for newer ntx devices, like Nooks and Tolinos.
- * based on https://github.com/koreader/koreader/issues/3517
- *
- * Thanks to @char11
- *
- *	val EINK_DITHER_COLOR_Y1 = 268435456;
- *	val EINK_DITHER_COLOR_Y4 = 0;
- *	val EINK_DITHER_MODE_DITHER = 256;
- *	val EINK_DITHER_MODE_NODITHER = 0;
- *	val EINK_MONOCHROME_MODE_MONOCHROME = 2048;
- *	val EINK_MONOCHROME_MODE_NOMONOCHROME = 0;
- *	val EINK_UPDATE_MODE_FULL = 32;
- *	val EINK_UPDATE_MODE_PARTIAL = 0;
- *	val EINK_WAIT_MODE_NOWAIT = 0;
- *	val EINK_WAIT_MODE_WAIT = 64;
- *	val EINK_WAVEFORM_MODE_A2 = 5;
- *	val EINK_WAVEFORM_MODE_AUTO = 4;
- *	val EINK_WAVEFORM_MODE_DU = 1;
- *	val EINK_WAVEFORM_MODE_GC16 = 2;
- *	val EINK_WAVEFORM_MODE_GC4 = 3;
- *	val EINK_WAVEFORM_MODE_GL16 = 6;
- *	val EINK_WAVEFORM_MODE_GLD16 = 8;
- *	val EINK_WAVEFORM_MODE_GLR16 = 7;
- *	val EINK_WAVEFORM_MODE_INIT = 0;
- */
-
 package org.koreader.launcher.device.epd.onyx
 
 import java.lang.Thread
