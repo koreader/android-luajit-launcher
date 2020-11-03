a79e48647f6c69dae3d79902ea8f474a74d4fb0c
Galunid <karolek1231456@gmail.com>
2020-11-03 16:32:35 +0100

    Implement missing interface methods

--- app/src/org/koreader/launcher/device/epd/qualcomm/QualcommOnyxEPDController.kt
+++ app/src/org/koreader/launcher/device/epd/qualcomm/QualcommOnyxEPDController.kt
@@ -5,6 +5,8 @@ package org.koreader.launcher.device.epd.qualcomm
 import org.koreader.launcher.interfaces.EPDInterface
 
 class QualcommOnyxEPDController : QualcommEPDController(), EPDInterface {
+    override fun resume() {}
+    override fun pause() {}
     override fun setEpdMode(targetView: android.view.View,
                             mode: Int, delay: Long,
                             x: Int, y: Int, width: Int, height: Int, epdMode: String?)
