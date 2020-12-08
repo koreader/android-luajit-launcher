12e089a1d654a26acc83623130353c0705e08aac
Martín Fdez <paziusss@gmail.com>
2020-12-08 21:00:59 +0100

    return to target API28

--- app/src/org/koreader/launcher/utils/Permissions.kt
+++ app/src/org/koreader/launcher/utils/Permissions.kt
@@ -1,6 +1,7 @@
 package org.koreader.launcher.utils
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
@@ -14,11 +15,12 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import org.koreader.launcher.R
 
+@SuppressLint("NewApi")
 object Permissions {
     private const val STORAGE_WRITE_ID = 1001
 
     fun hasStoragePermission(activity: Activity): Boolean {
-        return if (Build.VERSION.SDK_INT >= 30) {
+        return if (newStoragePermissions(activity)) {
             Environment.isExternalStorageManager()
         } else {
             hasPermissionGranted(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE)
@@ -26,20 +28,20 @@ object Permissions {
     }
 
     fun hasWriteSettingsPermission(context: Context): Boolean {
-        return if (Build.VERSION.SDK_INT >= 23) {
+        return if (newSettingsPermission())  {
             Settings.System.canWrite(context)
         } else true
     }
 
     fun isIgnoringBatteryOptimizations(context: Context): Boolean {
-        return if (Build.VERSION.SDK_INT >= 23) {
+        return if (newSettingsPermission()) {
             val pm = context.applicationContext.getSystemService(Context.POWER_SERVICE) as PowerManager
             pm.isIgnoringBatteryOptimizations(context.packageName)
         } else false
     }
 
     fun requestStoragePermission(activity: Activity) {
-        if (Build.VERSION.SDK_INT >= 30) {
+        return if (newStoragePermissions(activity)) {
             val intent = Intent(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION)
             val rationale = activity.resources.getString(R.string.warning_manage_storage)
             requestSpecialPermission(activity, intent, rationale, null, null)
@@ -50,23 +52,26 @@ object Permissions {
     }
 
     fun requestWriteSettingsPermission(activity: Activity, rationale: String, okButton: String, cancelButton: String) {
-        if (Build.VERSION.SDK_INT >= 23) {
+        if (newSettingsPermission()) {
             val intent = Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS)
             requestSpecialPermission(activity, intent, rationale, okButton, cancelButton)
         }
     }
 
     fun requestIgnoreBatteryOptimizations(activity: Activity, rationale: String, okButton: String, cancelButton: String) {
-        if (Build.VERSION.SDK_INT >= 23) {
+        if (newSettingsPermission()) {
             val intent = Intent(Settings.ACTION_IGNORE_BATTERY_OPTIMIZATION_SETTINGS)
             requestSpecialPermission(activity, intent, rationale, okButton, cancelButton)
         }
     }
+
+    @Suppress("SameParameterValue")
     private fun hasPermissionGranted(activity: Activity, perm: String): Boolean {
         val state = ContextCompat.checkSelfPermission(activity, perm)
         return (state == PackageManager.PERMISSION_GRANTED)
     }
 
+    @Suppress("SameParameterValue")
     private fun requestPermission(activity: Activity, perm: String, code: Int) {
         ActivityCompat.requestPermissions(activity, arrayOf(perm), code)
     }
@@ -87,5 +92,18 @@ object Permissions {
             }
             builder.create().show()
         }
+    }
+
+    private fun newStoragePermissions(activity: Activity): Boolean {
+        val targetSdk = activity.applicationContext.applicationInfo.targetSdkVersion
+        val runtimeSdk = Build.VERSION.SDK_INT
+        val minApi = Build.VERSION_CODES.R
+        return ((runtimeSdk >= minApi) and (targetSdk >= minApi))
+    }
+
+    private fun newSettingsPermission(): Boolean {
+        val runtimeSdk = Build.VERSION.SDK_INT
+        val minApi = Build.VERSION_CODES.M
+        return (runtimeSdk >= minApi)
     }
 }
