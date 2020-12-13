639158e05c7cdb8af9bfbbeed87fb41dada8962a
Martín Fernández <paziusss@gmail.com>
2020-12-13 12:12:20 +0100

    Fix for some dicts that have no dir entries (#274)

--- app/src/org/koreader/launcher/utils/ArchiveUtils.kt
+++ app/src/org/koreader/launcher/utils/ArchiveUtils.kt
@@ -8,43 +8,64 @@ import org.koreader.launcher.Logger
 import java.io.*
 
 object ArchiveUtils {
-    const val TAG = "ArchiveUtils"
+    private const val TAG = "ArchiveUtils"
 
-    fun untar(archive: String, extract_to: String): Boolean {
-        return try {
-            uncompress(archive, extract_to)
+    fun untar(archive: String, extract_to: String, deleteIfOk: Boolean = false): Boolean {
+        val success = try {
+            untar(archive, extract_to)
         } catch (e: IOException) {
             Logger.w(TAG, "exception uncompressing $archive: $e")
+            e.printStackTrace()
             false
         }
+
+        if (success and deleteIfOk) {
+            try {
+                File(archive).delete()
+            } catch (e: IOException) {
+                Logger.w(TAG, "exception deleting $archive: $e")
+                e.printStackTrace()
+            }
+        }
+        return success
     }
 
     @Throws(IOException::class)
-    private fun uncompress(archive: String, dest: String): Boolean {
+    private fun untar(archive: String, extract_to: String): Boolean {
         return getTarInput(archive)?.use {
-            val output = File(dest)
+            val output = File(extract_to)
             if (!output.exists()) {
-                Logger.v(TAG, "Creating dir ${output.absolutePath}")
+                Logger.i(TAG, "Creating destination dir: ${output.absolutePath}")
                 output.mkdir()
             }
             var tarEntry = it.nextTarEntry
             while (tarEntry != null) {
-                val destPath = File(dest, tarEntry.name)
-                if (tarEntry.isDirectory) {
-                    Logger.v(TAG, "Creating dir ${destPath.absolutePath}")
-                    destPath.mkdirs()
-                } else {
-                    Logger.v(TAG, "Creating file ${destPath.absolutePath}")
-                    destPath.createNewFile()
-                    val buffer = ByteArray(4096)
-                    val bufferOutput = BufferedOutputStream(FileOutputStream(destPath))
-                    var len: Int
-                    while (it.read(buffer).also { size -> len = size } != -1) {
-                        Logger.v(TAG, "Copying $len bytes to ${destPath.absolutePath}")
-                        bufferOutput.write(buffer, 0, len)
+                val destPath = File(extract_to, tarEntry.name)
+                destPath.parentFile?.let { parent ->
+                    if (!parent.exists()) {
+                        parent.mkdirs()
                     }
-                    Logger.v(TAG, "File ${destPath.absolutePath} created OK")
-                    bufferOutput.close()
+                }
+                var isOverride = false
+                if (!tarEntry.isDirectory) {
+                    if (destPath.exists()) {
+                        isOverride = true
+                        destPath.delete()
+                    }
+                    if (destPath.createNewFile()) {
+                        val buffer = ByteArray(4096)
+                        val bufferOutput = BufferedOutputStream(FileOutputStream(destPath))
+                        var len: Int
+                        while (it.read(buffer).also { size -> len = size } != -1) {
+                            bufferOutput.write(buffer, 0, len)
+                        }
+
+                        val mode = if (isOverride) "overwritten" else "created"
+                        Logger.v(TAG, "File ${destPath.absolutePath} $mode OK")
+                        bufferOutput.close()
+                    } else {
+                        Logger.w(TAG, "Failed creating file ${destPath.absolutePath}")
+                    }
                 }
                 tarEntry = it.nextTarEntry
             }
@@ -54,23 +75,22 @@ object ArchiveUtils {
     }
 
     private fun getTarInput(archive: String): TarArchiveInputStream? {
-        val ext = File(archive).extension
-        return if ((ext == "gz") or (ext == "tgz") or (ext == "bz2") or (ext == "lz")) {
-            Logger.i(TAG, "uncompressing $archive with compression $ext")
-            // ignore illegal values for group/userid, mode, device numbers and timestamp
-            TarArchiveInputStream(getCompressor(archive, ext) as InputStream, true)
+        val input = File(archive)
+        val validExtensions = arrayOf("bz2", "gz", "lz", "tgz")
+        return if (validExtensions.contains(input.extension)) {
+            Logger.i(TAG, "uncompressing $archive with compression ${input.extension}")
+            TarArchiveInputStream(
+                when (input.extension) {
+                    "bz2" -> BZip2CompressorInputStream(BufferedInputStream(FileInputStream(input)))
+                    "lz" -> LZMACompressorInputStream(BufferedInputStream(FileInputStream(input)))
+                    else -> GzipCompressorInputStream(BufferedInputStream(FileInputStream(input)))
+                },
+                // ignore illegal values for group/userid, mode, device numbers and timestamp
+                true
+            )
         } else {
             Logger.w(TAG, "invalid extension for $archive. Aborting")
             null
-        }
-    }
-
-    private fun getCompressor(archive: String, validExtension: String): Any {
-        val input = File(archive)
-        return when (validExtension) {
-            "bz2" -> BZip2CompressorInputStream(BufferedInputStream(FileInputStream(input)))
-            "lz" -> LZMACompressorInputStream(BufferedInputStream(FileInputStream(input)))
-            else -> GzipCompressorInputStream(BufferedInputStream(FileInputStream(input)))
         }
     }
 }
