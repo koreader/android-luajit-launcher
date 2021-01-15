5575e1fcd055e9e3b86d0937b8be217000ac62f4
NiLuJe <ninuje@gmail.com>
2021-01-15 19:21:30 +0100

    Fugly hack for C99 typedefs
    But at least this only requires tweaking the cdecl, not the generated
    output...

--- ffi-cdecl/elf_decl.c
+++ ffi-cdecl/elf_decl.c
@@ -15,20 +15,20 @@ cdecl_const(SHT_DYNAMIC)
 cdecl_const(DT_NEEDED)
 
 // ELFCLASS32
-cdecl_type(Elf32_Half)
-cdecl_type(Elf32_Word)
-cdecl_type(Elf32_Sword)
+cdecl_c99_type(Elf32_Half, uint16_t)
+cdecl_c99_type(Elf32_Word, uint32_t)
+cdecl_c99_type(Elf32_Sword, int32_t)
 
-cdecl_type(Elf32_Xword)
-cdecl_type(Elf32_Sxword)
+cdecl_c99_type(Elf32_Xword, uint64_t)
+cdecl_c99_type(Elf32_Sxword, int64_t)
 
-cdecl_type(Elf32_Addr)
+cdecl_c99_type(Elf32_Addr, uint32_t)
 
-cdecl_type(Elf32_Off)
+cdecl_c99_type(Elf32_Off, uint32_t)
 
-cdecl_type(Elf32_Section)
+cdecl_c99_type(Elf32_Section, uint16_t)
 
-cdecl_type(Elf32_Versym)
+cdecl_c99_type(Elf32_Versym, Elf32_Half)
 
 cdecl_type(Elf32_Ehdr)
 cdecl_type(Elf32_Shdr)
