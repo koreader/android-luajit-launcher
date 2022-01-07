f56753556742202b14c9a307d883f077998f3de1
Martín Fernández <975883+pazos@users.noreply.github.com>
2022-01-07 08:22:57 +0100

    Add Onyx Note 3 & Onyx Note 5 (#349)
    koreader/koreader#8482

--- app/src/main/java/org/koreader/launcher/device/LightsFactory.kt
+++ app/src/main/java/org/koreader/launcher/device/LightsFactory.kt
@@ -15,6 +15,7 @@ object LightsFactory {
                 }
                 DeviceInfo.LightsDevice.ONYX_KON_TIKI2,
                 DeviceInfo.LightsDevice.ONYX_DARWIN7,
+                DeviceInfo.LightsDevice.ONYX_NOTE3,
                 DeviceInfo.LightsDevice.ONYX_NOVA2,
                 DeviceInfo.LightsDevice.ONYX_NOVA_AIR -> {
                     logController("Onyx/Qualcomm")
