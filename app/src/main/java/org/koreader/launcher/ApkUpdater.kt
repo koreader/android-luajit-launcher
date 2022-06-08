2684714e4d5f0825d0b8f6fc41a74bc269d9f804
Martín Fdez <paziusss@gmail.com>
2022-06-09 01:10:16 +0200

    codacy

--- app/src/main/java/org/koreader/launcher/ApkUpdater.kt
+++ app/src/main/java/org/koreader/launcher/ApkUpdater.kt
@@ -5,7 +5,6 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Build
-import android.os.Environment
 import android.util.Log
 import androidx.core.content.FileProvider
 import java.io.File
