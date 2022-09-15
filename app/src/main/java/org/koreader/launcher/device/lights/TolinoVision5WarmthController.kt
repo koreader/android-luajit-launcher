8574b55744ed5490e190ff40b9d97da1a49e19dc
hasezoey <hasezoey@gmail.com>
2022-09-15 12:48:01 +0200

    style(TolinoVision5WarmthController): add comment that "MAX_WARMTH" can also be read from file

--- app/src/main/java/org/koreader/launcher/device/lights/TolinoVision5WarmthController.kt
+++ app/src/main/java/org/koreader/launcher/device/lights/TolinoVision5WarmthController.kt
@@ -19,7 +19,7 @@ class TolinoVision5WarmthController : Ioctl(), LightsInterface {
     companion object {
         private const val TAG = "Lights"
         private const val BRIGHTNESS_MAX = 255
-        private const val WARMTH_MAX = 10
+        private const val WARMTH_MAX = 10 // can also be read by ""/sys/class/backlight/lm3630a_led/max_color" on Vision5
         private const val MIN = 0
         private const val NTX_IO_FILE = "/dev/ntx_io"
         private const val NTX_WARMTH_ID = 248
