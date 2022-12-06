d952abcba5bc17caba5374330f42d75d0f4aaaca
Martín Fernández <paziusss@gmail.com>
2022-12-06 15:48:16 +0100

    onyx nova air 2

--- app/src/main/java/org/koreader/launcher/device/LightsFactory.kt
+++ app/src/main/java/org/koreader/launcher/device/LightsFactory.kt
@@ -39,6 +39,7 @@ object LightsFactory {
                 DeviceInfo.LightsDevice.ONYX_LEAF2,
                 DeviceInfo.LightsDevice.ONYX_NOTE_AIR2,
                 DeviceInfo.LightsDevice.ONYX_NOVA2,
+                DeviceInfo.LightsDevice.ONYX_NOVA_AIR_2,
                 DeviceInfo.LightsDevice.ONYX_NOVA_AIR_C,
                 DeviceInfo.LightsDevice.ONYX_POKE4 -> {
                     logController("Onyx/Sdk")
