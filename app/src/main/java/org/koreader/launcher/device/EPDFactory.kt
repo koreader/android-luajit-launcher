8887b764231d172e78a66a118112f74489792d7d
Álvaro Ferrero <alfedi3867@hotmail.com>
2023-04-02 16:49:55 +0200

    Add Tagus Gea

--- app/src/main/java/org/koreader/launcher/device/EPDFactory.kt
+++ app/src/main/java/org/koreader/launcher/device/EPDFactory.kt
@@ -86,7 +86,8 @@ object EPDFactory {
                 DeviceInfo.EinkDevice.ONYX_POKE4,
                 DeviceInfo.EinkDevice.ONYX_POKE4LITE,
                 DeviceInfo.EinkDevice.ONYX_POKE_PRO,
-                DeviceInfo.EinkDevice.ONYX_TAB_ULTRA -> {
+                DeviceInfo.EinkDevice.ONYX_TAB_ULTRA,
+		DeviceInfo.EinkDevice.TAGUS_GEA -> {
                     logController("Onyx/Qualcomm")
                     OnyxEPDController()
                 }
