473b2c894f39f27df05ac51ba961875bbdd8d563
Martín Fernández <paziusss@gmail.com>
2024-06-25 01:09:10 +0200

    copy on demand

--- app/src/main/java/org/koreader/launcher/extensions/UriExtensions.kt
+++ app/src/main/java/org/koreader/launcher/extensions/UriExtensions.kt
@@ -6,6 +6,7 @@ import android.net.Uri
 import android.os.Build
 import android.provider.MediaStore
 import android.system.Os
+import org.koreader.launcher.MainApp
 import java.io.File
 import java.io.FileNotFoundException
 import java.io.FileOutputStream
@@ -13,42 +14,18 @@ import java.io.IOException
 
 fun Uri.absolutePath(context: Context): String? {
     return when (this.scheme) {
-        ContentResolver.SCHEME_FILE -> {
-            this.path?.let { filePath -> File(filePath) }?.absolutePath
-        }
-        ContentResolver.SCHEME_CONTENT -> {
-            this.authority?.let { _ ->
-                try {
-                    context.contentResolver.openFileDescriptor(this, "r")?.use { parcel ->
-                        try {
-                            val file = File("/proc/self/fd/" + parcel.fd)
-                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                                Os.readlink(file.absolutePath)
-                            } else {
-                                file.canonicalPath
-                            }
-                        } catch (e: IOException) {
-                            null
-                        } catch (e: Exception) {
-                            null
-                        }
-                    }
-                } catch (e: FileNotFoundException) {
-                    e.printStackTrace()
-                    null
-                }
-            }
-        }
+        ContentResolver.SCHEME_FILE -> pathFromFile(this)
+        ContentResolver.SCHEME_CONTENT -> pathFromContent(this, context)
         else -> null
     }
 }
 
-fun Uri.toFile(context: Context, path: String) {
+fun Uri.toFile(context: Context, path: String): String? {
     if (this.scheme != ContentResolver.SCHEME_CONTENT) {
-        return
+        return null
     }
 
-    this.authority ?: return
+    this.authority ?: return null
     val nameColumn = arrayOf(MediaStore.MediaColumns.DISPLAY_NAME)
     val contentResolver = context.contentResolver
     val name: String? = contentResolver.query(this, nameColumn,
@@ -57,9 +34,9 @@ fun Uri.toFile(context: Context, path: String) {
         it.getString(it.getColumnIndex(nameColumn[0]))
     }
 
-    name ?: return
+    name ?: return null
 
-    context.contentResolver.openInputStream(this)?.use {
+    return context.contentResolver.openInputStream(this)?.use {
         try {
             val file = File(path, name)
             FileOutputStream(file).use { target ->
@@ -70,8 +47,48 @@ fun Uri.toFile(context: Context, path: String) {
                     len = it.read(buffer)
                 }
             }
+            file.absolutePath
         } catch (e: IOException) {
             e.printStackTrace()
+            null
         }
     }
+}
+
+private fun pathFromFile(uri: Uri): String? {
+    return uri.path?.let { filePath -> File(filePath) }?.absolutePath
+}
+
+private fun pathFromImportedFile(uri: Uri, context: Context): String? {
+    return uri.toFile(context, MainApp.app_storage_path)
+}
+private fun pathFromContent(uri: Uri, context: Context): String? {
+    val path = uri.authority?.let { _ ->
+        try {
+            context.contentResolver.openFileDescriptor(uri, "r")?.use { parcel ->
+                try {
+                    val file = File("/proc/self/fd/" + parcel.fd)
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                        Os.readlink(file.absolutePath)
+                    } else {
+                        file.canonicalPath
+                    }
+                } catch (e: IOException) {
+                    null
+                } catch (e: Exception) {
+                    null
+                }
+            }
+        } catch (e: FileNotFoundException) {
+            e.printStackTrace()
+            null
+        }
+    }
+
+    return path?.let { filePath ->
+        if (filePath.contains("/Android/data"))
+            pathFromImportedFile(uri, context)
+        else
+            filePath
+    }?: pathFromImportedFile(uri, context)
 }
