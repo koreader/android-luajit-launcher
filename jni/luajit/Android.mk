269ad5dcb1a7cee1f5bb970b23a6b10264d86548
Benoit Pierre <benoit.pierre@gmail.com>
2024-08-07 21:40:55 +0200

    add support for using an already compiled LuaJIT

--- jni/luajit/Android.mk
+++ jni/luajit/Android.mk
@@ -4,7 +4,12 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := luajit
+ifeq (,$(and $(LUAJIT_INC),$(LUAJIT_LIB)))
 LOCAL_SRC_FILES := build/$(TARGET_ARCH_ABI)/lib/libluajit.so
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/build/$(TARGET_ARCH_ABI)/include/luajit-2.1
+else
+LOCAL_SRC_FILES := $(LUAJIT_LIB)
+LOCAL_EXPORT_C_INCLUDES := $(LUAJIT_INC)
+endif
 
 include $(PREBUILT_SHARED_LIBRARY)
