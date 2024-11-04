e828a127dd8da8fa35f9c562380fac8914bd4c99
Benoit Pierre <benoit.pierre@gmail.com>
2024-11-04 20:12:21 +0100

    rework DeviceInfo.kt
    Reduce duplication to make it easier to maintain.
    
    Fix the following issues:
    
    - possible problem with `EINK` value on `TOLINO_EPOS3` devices
      (since both `TOLINO` and `TOLINO_EPOS3` booleans would be set)
    - `EINK` was never set to `EinkDevice.TOLINO_VISION6`,
      but checked in `device/EPDFactory.kt`
    - same with `HANVON_960` & `HYREAD_MINI6`
    - `LIGHTS` was never set to `LightsDevice.ONYX_NOTE_PRO`,
      but checked in `device/LightsFactory.kt`
    - `LIGHTS` could be set to `LightsDevice.ONYX_PALMA`,
      but was never checked in `device/LightsFactory.kt`
    
    Note: also drop device properties: `is_boyue` (unused) & `is_tolino`
    (only used to add direction pad mappings, details were added to the
    relevant wiki page instead), and no hardware rotation quirk (which
    was an incomplete emulator only workaround).

--- app/src/main/java/org/koreader/launcher/device/LightsFactory.kt
+++ app/src/main/java/org/koreader/launcher/device/LightsFactory.kt
@@ -8,81 +8,90 @@ object LightsFactory {
     private const val TAG = "Lights"
     val lightsController: LightsInterface
         get() {
-            return when (DeviceInfo.LIGHTS) {
-                DeviceInfo.LightsDevice.NOOK_GL4 -> {
-                    logController("TolinoRoot")
-                    TolinoRootController()
+            return when (DeviceInfo.ID) {
+                DeviceInfo.Id.BOYUE_S62,
+                -> {
+                    logController("Boyue S62")
+                    BoyueS62RootController()
                 }
-                DeviceInfo.LightsDevice.CREMA_0710C,
-                DeviceInfo.LightsDevice.CREMA_CARTA_G,
-                DeviceInfo.LightsDevice.MEEBOOK_P6,
-                DeviceInfo.LightsDevice.RIDI_PAPER_3,
-                DeviceInfo.LightsDevice.TOLINO_EPOS1,
-                DeviceInfo.LightsDevice.TOLINO_EPOS2,
-                DeviceInfo.LightsDevice.TOLINO_SHINE3,
-                DeviceInfo.LightsDevice.TOLINO_VISION4,
-                DeviceInfo.LightsDevice.TOLINO_VISION5 -> {
-                    logController("TolinoNTX")
-                    TolinoNtxController()
-                }
-                DeviceInfo.LightsDevice.TOLINO_PAGE2 -> {
-                    logController("TolinoNTXNoWarmth")
-                    TolinoNtxNoWarmthController()
-                }
-                DeviceInfo.LightsDevice.ONYX_DARWIN7,
-                DeviceInfo.LightsDevice.ONYX_EDISON,
-                DeviceInfo.LightsDevice.ONYX_FAUST3,
-                DeviceInfo.LightsDevice.ONYX_KON_TIKI2,
-                DeviceInfo.LightsDevice.ONYX_LEAF,
-                DeviceInfo.LightsDevice.ONYX_LOMONOSOV,
-                DeviceInfo.LightsDevice.ONYX_NOTE3,
-                DeviceInfo.LightsDevice.ONYX_NOTE_AIR,
-                DeviceInfo.LightsDevice.ONYX_NOTE_PRO,
-                DeviceInfo.LightsDevice.ONYX_NOVA3,
-                DeviceInfo.LightsDevice.ONYX_NOVA_PRO,
-                DeviceInfo.LightsDevice.ONYX_POKE3,
-                DeviceInfo.LightsDevice.ONYX_POKE_PRO -> {
-                    logController("Onyx/Qualcomm")
-                    OnyxWarmthController()
-                }
-                DeviceInfo.LightsDevice.ONYX_LEAF2,
-                DeviceInfo.LightsDevice.ONYX_DARWIN9,
-                DeviceInfo.LightsDevice.ONYX_NOTE_AIR2,
-                DeviceInfo.LightsDevice.ONYX_NOTE_X2,
-                DeviceInfo.LightsDevice.ONYX_NOTE4,
-                DeviceInfo.LightsDevice.ONYX_NOVA,
-                DeviceInfo.LightsDevice.ONYX_NOVA2,
-                DeviceInfo.LightsDevice.ONYX_NOVA_AIR_2,
-                DeviceInfo.LightsDevice.ONYX_NOVA_AIR_C,
-                DeviceInfo.LightsDevice.ONYX_POKE4,
-                DeviceInfo.LightsDevice.ONYX_POKE4LITE,
-                DeviceInfo.LightsDevice.ONYX_TAB_ULTRA -> {
-                    logController("Onyx/Sdk")
-                    OnyxSdkLightsController()
-                }
-                DeviceInfo.LightsDevice.ONYX_JDREAD,
-                DeviceInfo.LightsDevice.ONYX_NOVA3_COLOR,
-                DeviceInfo.LightsDevice.TAGUS_GEA -> {
-                    logController("Onyx color")
-                    OnyxColorController()
-                }
-                DeviceInfo.LightsDevice.ONYX_GO_COLOR7,
-                DeviceInfo.LightsDevice.ONYX_NOVA_AIR,
-                DeviceInfo.LightsDevice.ONYX_PAGE,
-                DeviceInfo.LightsDevice.ONYX_POKE5,
-                DeviceInfo.LightsDevice.ONYX_TAB_ULTRA_C -> {
+                DeviceInfo.Id.ONYX_GO_COLOR7,
+                DeviceInfo.Id.ONYX_NOVA_AIR,
+                DeviceInfo.Id.ONYX_PAGE,
+                DeviceInfo.Id.ONYX_POKE5,
+                DeviceInfo.Id.ONYX_TAB_ULTRA_C,
+                -> {
                     logController("Onyx Adb")
                     OnyxAdbLightsController()
                 }
-                DeviceInfo.LightsDevice.ONYX_C67,
-                DeviceInfo.LightsDevice.ONYX_MAGICBOOK,
-                DeviceInfo.LightsDevice.ONYX_MONTECRISTO3 -> {
+                DeviceInfo.Id.ONYX_C67,
+                DeviceInfo.Id.ONYX_MAGICBOOK,
+                DeviceInfo.Id.ONYX_MONTECRISTO3,
+                -> {
                     logController("Onyx C67")
                     OnyxC67Controller()
                 }
-                DeviceInfo.LightsDevice.BOYUE_S62 -> {
-                    logController("Boyue S62")
-                    BoyueS62RootController()
+                DeviceInfo.Id.ONYX_JDREAD,
+                DeviceInfo.Id.ONYX_NOVA3_COLOR,
+                DeviceInfo.Id.TAGUS_GEA,
+                -> {
+                    logController("Onyx Color")
+                    OnyxColorController()
+                }
+                DeviceInfo.Id.ONYX_DARWIN7,
+                DeviceInfo.Id.ONYX_EDISON,
+                DeviceInfo.Id.ONYX_FAUST3,
+                DeviceInfo.Id.ONYX_KON_TIKI2,
+                DeviceInfo.Id.ONYX_LEAF,
+                DeviceInfo.Id.ONYX_LOMONOSOV,
+                DeviceInfo.Id.ONYX_NOTE3,
+                DeviceInfo.Id.ONYX_NOTE_AIR,
+                DeviceInfo.Id.ONYX_NOTE_PRO,
+                DeviceInfo.Id.ONYX_NOVA3,
+                DeviceInfo.Id.ONYX_NOVA_PRO,
+                DeviceInfo.Id.ONYX_POKE3,
+                DeviceInfo.Id.ONYX_POKE_PRO,
+                -> {
+                    logController("Onyx/Qualcomm")
+                    OnyxWarmthController()
+                }
+                DeviceInfo.Id.ONYX_DARWIN9,
+                DeviceInfo.Id.ONYX_LEAF2,
+                DeviceInfo.Id.ONYX_NOTE4,
+                DeviceInfo.Id.ONYX_NOTE_AIR2,
+                DeviceInfo.Id.ONYX_NOTE_X2,
+                DeviceInfo.Id.ONYX_NOVA,
+                DeviceInfo.Id.ONYX_NOVA2,
+                DeviceInfo.Id.ONYX_NOVA_AIR_2,
+                DeviceInfo.Id.ONYX_NOVA_AIR_C,
+                DeviceInfo.Id.ONYX_POKE4,
+                DeviceInfo.Id.ONYX_POKE4LITE,
+                DeviceInfo.Id.ONYX_TAB_ULTRA,
+                -> {
+                    logController("Onyx/Sdk")
+                    OnyxSdkLightsController()
+                }
+                DeviceInfo.Id.CREMA_0710C,
+                DeviceInfo.Id.CREMA_CARTA_G,
+                DeviceInfo.Id.MEEBOOK_P6,
+                DeviceInfo.Id.RIDI_PAPER_3,
+                DeviceInfo.Id.TOLINO_EPOS1,
+                DeviceInfo.Id.TOLINO_EPOS2,
+                DeviceInfo.Id.TOLINO_SHINE3,
+                DeviceInfo.Id.TOLINO_VISION4,
+                DeviceInfo.Id.TOLINO_VISION5,
+                -> {
+                    logController("TolinoNTX")
+                    TolinoNtxController()
+                }
+                DeviceInfo.Id.TOLINO_PAGE2,
+                -> {
+                    logController("TolinoNTXNoWarmth")
+                    TolinoNtxNoWarmthController()
+                }
+                DeviceInfo.Id.NOOK_GL4,
+                -> {
+                    logController("TolinoRoot")
+                    TolinoRootController()
                 }
                 else -> {
                     logController("Generic")
