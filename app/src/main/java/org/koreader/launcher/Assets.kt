2f2030c3972a1d5e7ed95e5b98d64394671f4a65
Benoit Pierre <benoit.pierre@gmail.com>
2024-11-10 17:17:29 +0100

    assets: minor cleanup
    Remove unused import.

--- app/src/main/java/org/koreader/launcher/Assets.kt
+++ app/src/main/java/org/koreader/launcher/Assets.kt
@@ -10,7 +10,6 @@ import android.view.ViewGroup
 import android.widget.ProgressBar
 import androidx.core.content.ContextCompat
 import org.koreader.launcher.extensions.pruneCacheDir
-import org.koreader.launcher.extensions.symlink
 import java.io.*
 
 class Assets {
