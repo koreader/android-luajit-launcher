849d118cf07895471f0b9ebe72b03ddc9d137acd
hugleo <hugocanalli@gmail.com>
2024-11-27 20:21:00 -0300

    for the sake...

--- app/src/main/java/org/koreader/launcher/device/LightsFactory.kt
+++ app/src/main/java/org/koreader/launcher/device/LightsFactory.kt
@@ -74,7 +74,6 @@ object LightsFactory {
                 DeviceInfo.Id.CREMA_CARTA_G,
                 DeviceInfo.Id.MEEBOOK_P6,
                 DeviceInfo.Id.RIDI_PAPER_3,
-                DeviceInfo.Id.TOLINO_EPOS1,
                 DeviceInfo.Id.TOLINO_SHINE3,
                 DeviceInfo.Id.TOLINO_VISION4,
                 DeviceInfo.Id.TOLINO_VISION5,
@@ -88,6 +87,7 @@ object LightsFactory {
                     TolinoNtxNoWarmthController()
                 }
                 DeviceInfo.Id.NOOK_GL4,
+                DeviceInfo.Id.TOLINO_EPOS1,
                 DeviceInfo.Id.TOLINO_EPOS2,
                 -> {
                     logController("TolinoRoot")
