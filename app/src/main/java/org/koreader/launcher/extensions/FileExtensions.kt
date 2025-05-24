b411d6dd3e7541d56becda8f552f54f79ae80f54
Benoit Pierre <benoit.pierre@gmail.com>
2025-05-24 23:18:59 +0200

    drop untar implementation
    Not needed anymore.

--- app/src/main/java/org/koreader/launcher/extensions/FileExtensions.kt
+++ app/src/main/java/org/koreader/launcher/extensions/FileExtensions.kt
@@ -3,11 +3,6 @@ package org.koreader.launcher.extensions
 import android.annotation.SuppressLint
 import android.os.Build
 import android.system.Os
-import org.apache.commons.compress.archivers.tar.TarArchiveInputStream
-import org.apache.commons.compress.compressors.bzip2.BZip2CompressorInputStream
-import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream
-import org.apache.commons.compress.compressors.lzma.LZMACompressorInputStream
-import org.apache.commons.compress.utils.IOUtils
 import java.io.*
 
 @SuppressLint("DiscouragedPrivateApi")
@@ -51,71 +46,5 @@ fun File.write(value: Int) {
         writeText(value.toString())
     } catch (e: Exception) {
         e.printStackTrace()
-    }
-}
-
-fun File.uncompress(extractTo: String, deleteIfOk: Boolean = false): Boolean {
-    val success = try {
-        uncompress(this.absolutePath, extractTo)
-    } catch (e: IOException) {
-        e.printStackTrace()
-        false
-    }
-
-    if (success and deleteIfOk) {
-        try {
-            delete()
-        } catch (e: IOException) {
-            e.printStackTrace()
-        }
-    }
-    return success
-}
-
-@Throws(IOException::class)
-private fun uncompress(archive: String, extractTo: String): Boolean {
-    return getTarInput(archive)?.use {
-        val output = File(extractTo)
-        if (!output.exists()) {
-            output.mkdir()
-        }
-        var tarEntry = it.nextTarEntry
-        while (tarEntry != null) {
-            val destPath = File(extractTo, tarEntry.name)
-            destPath.parentFile?.let { parent ->
-                if (!parent.exists()) {
-                    parent.mkdirs()
-                }
-            }
-            if (!tarEntry.isDirectory) {
-                if (destPath.exists()) {
-                    destPath.delete()
-                }
-                if (destPath.createNewFile()) {
-                    FileOutputStream(destPath).use { out -> IOUtils.copy(it, out, 4096) }
-                }
-            }
-            tarEntry = it.nextTarEntry
-        }
-        it.close()
-        true
-    } ?:false
-}
-
-private fun getTarInput(archive: String): TarArchiveInputStream? {
-    val input = File(archive)
-    val validExtensions = arrayOf("bz2", "gz", "lz", "tgz")
-    return if (validExtensions.contains(input.extension)) {
-        TarArchiveInputStream(
-            when (input.extension) {
-                "bz2" -> BZip2CompressorInputStream(BufferedInputStream(FileInputStream(input)))
-                "lz" -> LZMACompressorInputStream(BufferedInputStream(FileInputStream(input)))
-                else -> GzipCompressorInputStream(BufferedInputStream(FileInputStream(input)))
-            },
-            // ignore illegal values for group/userid, mode, device numbers and timestamp
-            true
-        )
-    } else {
-        null
     }
 }
